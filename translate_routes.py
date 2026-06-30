5a14d0854000b803ea4e13b3536419375d680199
Munira <makhmudovamuni007@gmail.com>
2026-06-30 19:22:24 +0500

    malumotlar secret qilindi

--- translate_routes.py
+++ translate_routes.py
@@ -13,50 +13,6 @@ translate_router = APIRouter(
 recognizer = sr.Recognizer()
 
 
-@translate_router.post("/voice1", status_code=status.HTTP_200_OK)
-async def translate_voice(
-        file: UploadFile = File(...),
-        Authorize: AuthJWT = Depends()
-):
-    # try:
-    #     # JWT Token borligini va to'g'riligini tekshirish
-    #     Authorize.jwt_required()
-    # except Exception:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_401_UNAUTHORIZED,
-    #         detail="Ruxsat berilmagan! Avval tizimga kiring."
-    #     )
-
-    # Kelgan audio faylni o'qish
-    audio_bytes = await file.read()
-
-    try:
-        # Audioni SpeechRecognition formati bo'yicha yuklaymiz
-        audio_data = sr.AudioData(audio_bytes, sample_rate=16000, sample_width=2)
-
-        # 1. Ovozni o'zbekcha matnga aylantirish
-        text = recognizer.recognize_google(audio_data, language="uz-UZ")
-
-        if not text:
-            raise HTTPException(status_code=400, detail="Ovozda hech qanday so'z aniqlanmadi.")
-
-        # 2. Matnni ingliz tiliga tarjima qilish
-        translated = GoogleTranslator(source='uz', target='en').translate(text)
-
-        return {
-            "status": "success",
-            "original_text": text,
-            "translated_text": translated
-        }
-
-    except sr.UnknownValueError:
-        raise HTTPException(
-            status_code=400,
-            detail="Ovoz formati mos kelmadi yoki tushunarsiz. Aniqroq gapirib ko'ring."
-        )
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Xatolik: {str(e)}")
-
 @translate_router.get("/")
 def translate(text: str):
     print(text)
